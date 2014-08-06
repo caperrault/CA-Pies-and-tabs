de628722ee5de59c595c7962fc85dc246d90915d
Charles-Antoine Perrault <perrault.ca@gmail.com>
2014-08-06 15:12:36 -0700

    transfer

---
+++ shawn.js
@@ -0,0 +1,31 @@
+var fillProperty = "ac_tot"
+
+.attr("class", ...)
+.call(updateFill)
+...
+
+function updateFill(selection) {
+  selection.attr("fill", function(d) {
+    return quantize(d.properties[fillProperty]);
+  });
+}
+
+function setFillProperty(property) {
+  fillProperty = property;
+  svg.selectAll("counties2").call(updateFill);
+}
+
+// <input type="radio" value="ac_tot"
+
+radio.on("click", function() {
+  setFillProperty(this.value);
+});
+
+d3.csv("blah.csv", function(err, data) {
+  container.style("display", "none");
+})
+
+function setValue(value) {
+  container.style("display", null);
+}
+})
